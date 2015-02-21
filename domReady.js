b672459a75cde96ea2342d432ea9ee7cc69a3019
Joe Buffington <sharepointcentric@users.noreply.github.com>
2015-02-21 10:36:16 -0800

    Update domReady.js

--- domReady.js
+++ domReady.js
@@ -1,7 +1,7 @@
-var domReady = (function () {
-    var doc = document, isReady = 0, queue = [], fnc, fn,
-        _timer, modern = doc.addEventListener, doScroll = 0,
-        re = modern ? /^.(?!oading)/ : /^c/, 
+var domReady = (function (win, doc) {
+    var isReady = 0, queue = [], fnc, fn, _timer,
+        modern = doc.addEventListener, doScroll = !modern, 
+        re = modern ? /^.(?!oading)/ : /^c/,
         add = modern ? 'addEventListener' : 'attachEvent',
         event = modern ? 'DOMContentLoaded' : 'onreadystatechange',
         remove = modern ? 'removeEventListener' : 'detachEvent';
@@ -15,20 +15,19 @@ var domReady = (function () {
 
     if(re.test(doc.readyState)) ready();
     else {
-        try {doScroll = !window.frameElement && doc.documentElement.doScroll;} catch (e){}
+        doc[add](event, fn = function () {
+            if(!re.test(doc.readyState)) return;
+            doc[remove](event, fn, false);
+            ready();
+        }, false);
+        try {doScroll = doScroll && !win.frameElement && doc.documentElement.doScroll;} catch (e){}
         if(doScroll) {
             _timer = setInterval(function() {
                 try {doc.body.doScroll('up');ready();} catch(e) {}
             }, 10);
         }
-        doc[add](event, fn = function () {
-            if(re.test(doc.readyState)) {
-                doc[remove](event, fn, false);
-                ready();    
-            }
-        }, false);
     }
     return function (cb) {
         isReady ? cb() : queue.push(cb);
     };
-}());
+}(window, document));
